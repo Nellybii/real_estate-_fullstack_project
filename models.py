f0a5b22f549a591614bd39b4e973cb0f28f39a39
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-01-11 14:27:38 +0300

    creating models

--- models.py
+++ models.py
@@ -14,6 +14,27 @@ class User(db.Model):
     phone=db.Column(db.String, nullable= False, unique=True)
     role=db.Column(db.Text, nullable= False)
     password=db.Column(db.String, nullable= False)
-    created_at=db.Column(db.TIMESTAMP)
+    created_at=db.Column(db.TIMESTAMP, server_default=db.func.now())
+    updated_at= db.Column(db.TIMESTAMP, onupdate=db.func.now())
+    # deleted_at= db.Column(db.TIMESTAMP)
+
+class LocationModel(db.Model):
+    __tablename__="locations"
+
+    id= db.Column(db.Integer, primary_key= True)
+    name=db.Column(db.Text, nullable= False)
+    created_at=db.Column(db.TIMESTAMP, server_default=db.func.now())
+    updated_at= db.Column(db.TIMESTAMP, onupdate=db.func.now())
+
+# class Property(db.model):
+#     __tablename__ = "properties"
+
+#     id= db.Column(db.Integer, primary_key= True)
+#     name=db.Column(db.Text, nullable= False)
+#     description=db.Column(db.String, nullable= False)
+#     listing_price= db.Column(db.Integer, nullable= False)
+#     location=db.Column(db.Text, nullable= False)
+#     created_at=db.Column(db.TIMESTAMP, server_default=db.func.now())
+#     updated_at= db.Column(db.TIMESTAMP, onupdate=db.func.now())
 
 
